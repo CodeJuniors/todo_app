6b71cbe15e04f8de0e2d0e87f453a5267dcd7283
sossygebremeskel <sossygebremeskel@gmail.com>
2024-11-24 22:29:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
+
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", handleTodoActions);
+filterOption.addEventListener("change", filterTodo);
